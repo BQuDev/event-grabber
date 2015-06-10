8bf162181849a9fca87569dd7bd6d122101645c3
BQumobile <bqu.mobile.dev@gmail.com>
2015-06-10 09:18:10 +0530

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'forge',
 			'username'  => 'forge',
-			'password'  => 'uhvvZYarSIGy5B8oWNJB',
+			'password'  => 'OWubZ0eEz7Zc45d70AbU',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
